aca7b0fcb67398e0c2a47a608e99a8e1b9f677c7
Kurtis Bowen <1750019@y115.org>
2016-08-24 14:19:12 -0500

    Initial Minesweeper files
    Signed-off-by: Kurtis Bowen <1750019@y115.org>

---
+++ 1.6.2-Minesweeper/src/minesweep/Main.java
@@ -0,0 +1,14 @@
+package minesweep;
+
+import java.util.Scanner;
+
+public class Main {
+
+	Scanner in = new Scanner(System.in);
+	boolean[] board;
+	
+	public static void main(String[] args) {
+
+	}
+
+}
