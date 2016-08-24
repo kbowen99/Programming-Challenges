3d081f8d37dcd72a013b71fc310a1180b82b178f
Kurtis Bowen <1750019@y115.org>
2016-08-24 14:39:06 -0500

    a Start

--- 1.6.2-Minesweeper/src/minesweep/Main.java
+++ 1.6.2-Minesweeper/src/minesweep/Main.java
@@ -4,11 +4,36 @@ import java.util.Scanner;
 
 public class Main {
 
-	Scanner in = new Scanner(System.in);
-	boolean[] board;
+	static Scanner sc = new Scanner(System.in);
+	static boolean[][] board = null;
+	static int n;
+	static int m;
 	
 	public static void main(String[] args) {
+		String input = "";
+		while (sc.hasNext()){
+			input = sc.nextLine();
+			
+			//Try to take input for board
+			if (board == null){
+				try{
+					if (Integer.parseInt((char)(input.getBytes()[0]) + "") > 0){
+						n = Integer.parseInt((char)(input.getBytes()[0]) + "");
+					}
+					if (Integer.parseInt((char)(input.getBytes()[3]) + "") > 0){
+						m = Integer.parseInt((char)(input.getBytes()[3]) + "");
+					}
+					if (n>0 && m>0){
+						board = new boolean[n][m];
+						p("")
+					}
+				} catch (Exception e){}
+			}
 
+		};
 	}
-
+	
+	private static Object p(Object p){
+		System.out.println(p); return p;
+	}
 }
