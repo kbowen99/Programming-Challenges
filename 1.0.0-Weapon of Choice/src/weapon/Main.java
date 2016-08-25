ba4c2676e400b9778b731a2a79994e2f895b71f9
Kurtis Bowen <1750019@y115.org>
2016-08-25 15:07:57 -0500

    Actually Working

--- 1.0.0-Weapon of Choice/src/weapon/Main.java
+++ 1.0.0-Weapon of Choice/src/weapon/Main.java
@@ -7,53 +7,127 @@ import java.util.Scanner;
 public class Main {
 
 	static String[] zero = {
-			"┌-┐",
+			"┌─┐",
 			"│ │",
-			"└-┘"
+			"└─┘"
 	};
-	String[] one = {
+	static String[] one = {
 			"  ┐",
 			"  │",
 			"  │"
 	};
-	String[] two = {
-			"--┐",
-			"┌-┘",
-			"└--"
+	static String[] two = {
+			"──┐",
+			"┌─┘",
+			"└──"
 	};
-	String[] three = {
-			"--┐",
-			" -┤",
-			"--┘"
+	static String[] three = {
+			"──┐",
+			" ─┤",
+			"──┘"
 	};
-	String[] four = {
+	static String[] four = {
 			"│ │",
-			"└-┤",
+			"└─┤",
 			"  │"
 	};
-	String[] five = {
-			"┌--",
-			"└-┐",
-			"--┘"
+	static String[] five = {
+			"┌──",
+			"└─┐",
+			"──┘"
 	};
-	String[] six = {
-			"┌--",
-			"├-┐",
-			"└-┘"
+	static String[] six = {
+			"┌──",
+			"├─┐",
+			"└─┘"
 	};
-	public static void main(String[] args) {
-		System.out.println(zero[0]);
+	static String[] seven = {
+			"──┐",
+			"  │",
+			"  │"
+	};
+	static String[] eight = {
+			"┌─┐",
+			"├─┤",
+			"└─┘"
+	};
+	static String[] nine = {
+			"┌─┐",
+			"└─┤",
+			"  │"			
+	};
+	static String[] colon = {
+			" ■ ",
+			"   ",
+			" ■ "
+	};
+	
+	static String[][] display = new String[][]{zero,one,two,three,four,five,six,seven,eight,nine,colon};
+	static String[] pOut = {"","",""};
+	
+	
+	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		String input = "";
 		while (!input.equals("q")){
 			input = sc.nextLine();
 			if (input.toLowerCase().equals("time")){
-				System.out.println((new SimpleDateFormat("HH:mm:ss")).format(Calendar.getInstance().getTime()));
-				
+				printCurrTime();
+			}
+			if (input.toLowerCase().equals("ctime")){
+				while (!input.equals("q")){
+					//input = sc.nextLine();
+					printCurrTime();
+				}
 			}
 		}
 		sc.close();
 
 	}
-
+	
+	private static void printCurrTime(){
+		pOut[0] = "";
+		pOut[1] = "";
+		pOut[2] = "";
+		String time = (new SimpleDateFormat("HH:mm:ss.SSS")).format(Calendar.getInstance().getTime());
+		print7Seg(time);
+	}
+	
+	private static void print7Seg(String sInput){
+		char[] toPrint = sInput.toCharArray();
+		for (int i = 0; i < toPrint.length; i++){
+			addChar(toPrint[i]);
+		}
+		printArray(pOut);
+	}
+	
+	private static void addChar(char chr){
+		try {
+			addRawNum(Integer.parseInt(chr + ""));
+			addSpace(pOut);
+		} catch (Exception e) {
+			addRawNum(10);
+			addSpace(pOut);
+		}
+	}
+	
+	private static void addRawNum(int numArray){
+		for (int i = 0; i < 3; i++){
+			pOut[i] += display[numArray][i];
+		}
+		addSpace(pOut);
+	}
+	
+	private static void addSpace(String[] toSpace){
+		for (int i = 0; i < toSpace.length; i++){
+			toSpace[i] += " ";
+		}
+	}
+	
+	private static void printArray(String[] arr){
+		for (int i = 0; i < arr.length; i++){
+			System.out.println(arr[i]);
+		}
+	}
 }
+
