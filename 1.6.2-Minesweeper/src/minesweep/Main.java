ebe7c9bc7d95c1952a2dcb33c06ce8cf8dd7186b
Kurtis Bowen <1750019@y115.org>
2016-08-26 14:39:08 -0500

    Basic Input of multiple arrays

--- 1.6.2-Minesweeper/src/minesweep/Main.java
+++ 1.6.2-Minesweeper/src/minesweep/Main.java
@@ -1,34 +1,53 @@
 package minesweep;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
 	static Scanner sc = new Scanner(System.in);
-	static boolean[][] board = null;
+	//static boolean[][] board = null;
 	static int n;
 	static int m;
+	static int field = 0;
+	static ArrayList<boolean[][]> fields = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		String input = "";
 		while (sc.hasNext()){
 			input = sc.nextLine();
 			
-			//Try to take input for board
-			if (board == null){
 				try{
 					if (Integer.parseInt((char)(input.getBytes()[0]) + "") > 0){
-						n = Integer.parseInt((char)(input.getBytes()[0]) + "");
-					}
-					if (Integer.parseInt((char)(input.getBytes()[3]) + "") > 0){
-						m = Integer.parseInt((char)(input.getBytes()[3]) + "");
+						int index = input.indexOf(" ");
+						n = Integer.parseInt(input.substring(0, index));
+						m = Integer.parseInt(input.substring(index + 1));
 					}
 					if (n>0 && m>0){
-						board = new boolean[n][m];
-						p("")
+						fields.add(new boolean[n][m]);
+						p("Field #" + (field + 1) + ":");
+						n = 0;
+						m = 0;
+						
+						for (int r = 0; r < fields.get(field).length; r++){
+							input = sc.nextLine();
+							for(int c = 0; c < fields.get(field)[0].length; c++){
+								String tmp = (char)(input.getBytes()[c]) + "";
+								fields.get(field)[r][c] = (tmp.equals("*") ? true : false);
+							}
+						}
+						
+						for (int r = 0; r < fields.get(field).length; r++){
+							for(int c = 0; c < fields.get(field)[0].length; c++){
+								System.out.print((fields.get(field)[r][c] ? "T" : "F"));
+							}
+							System.out.println();
+						}
+						
+						field++;
 					}
 				} catch (Exception e){}
-			}
+			
 
 		};
 	}
