86d601abb00b5b750f2cbdd9b6060a6b753bb566
kbowen99 <kurtisbowen99@gmail.com>
2016-08-30 21:12:19 -0500

    Little bit cleaner, little bit meaner

--- 1.6.2-Minesweeper/src/minesweep/Main.java
+++ 1.6.2-Minesweeper/src/minesweep/Main.java
@@ -9,8 +9,9 @@ import java.util.Scanner;
 @SuppressWarnings("unused")
 public class Main {
 
+	static final Boolean DEBUG = false; //To Debug, or not to debug, always the question
+	
 	static Scanner sc = new Scanner(System.in);
-	//static boolean[][] board = null;
 	static int n;
 	static int m;
 	static int field = 0;
@@ -18,87 +19,74 @@ public class Main {
 	
 	public static void main(String[] args) {
 		try {
-			System.setOut(new PrintStream(new FileOutputStream("out.txt")));
-		} catch (Exception e1) {
-			e1.printStackTrace();
-		}
+			if (DEBUG)
+				System.setOut(new PrintStream(new FileOutputStream("out.txt")));
+		} catch (Exception e) {}
+		
 		String input = "";
 		while (sc.hasNextLine()){
 			input = sc.nextLine();
-				try{
-					if (Integer.parseInt((char)(input.getBytes()[0]) + "") > -1){
-						int index = input.indexOf(" ");
-						n = Integer.parseInt(input.substring(0, index));
-						m = Integer.parseInt(input.substring(index + 1));
-						if (n == 0|| m == 0){
-							break;
+			try{
+				if (Integer.parseInt((char)(input.getBytes()[0]) + "") > -1){
+					int index = input.indexOf(" ");
+					n = Integer.parseInt(input.substring(0, index));
+					m = Integer.parseInt(input.substring(index + 1));
+					if (n == 0|| m == 0){
+						break;
+					}
+				}
+				if (n>0 && m>0){
+					fields.add(new boolean[n][m]);
+					n = 0; m = 0;
+					
+					if (field != 0)
+						System.out.println();
+					
+					System.out.println("Field #" + (field + 1) + ":");
+					
+					for (int r = 0; r < fields.get(field).length; r++){
+						input = sc.nextLine();
+						for(int c = 0; c < fields.get(field)[0].length; c++){
+							String tmp = (char)(input.getBytes()[c]) + "";
+							fields.get(field)[r][c] = (tmp.equals("*") ? true : false);
 						}
 					}
-					if (n>0 && m>0){
-						fields.add(new boolean[n][m]);
-						n = 0;
-						m = 0;
-						
-						if (field != 0){
-							System.out.println();
-						}
-						p("Field #" + (field + 1) + ":");
-						
-						for (int r = 0; r < fields.get(field).length; r++){
-							input = sc.nextLine();
-							for(int c = 0; c < fields.get(field)[0].length; c++){
-								String tmp = (char)(input.getBytes()[c]) + "";
-								fields.get(field)[r][c] = (tmp.equals("*") ? true : false);
+					
+					for (int r = 0; r < fields.get(field).length; r++){
+						for(int c = 0; c < fields.get(field)[0].length; c++){
+							if (fields.get(field)[r][c]){
+								System.out.print("*");
+							}else{
+								int count = 0;
+								boolean cZero = (c == 0);
+								boolean cMax = ((c + 1) == fields.get(field)[r].length);
+								boolean rZero = (r < 1);
+								boolean rMax = ((r + 1) == fields.get(field).length);
+								if (!rZero && fields.get(field)[r-1][c])
+									count++;
+								if (!rZero && !cZero && fields.get(field)[r-1][c-1])
+									count++;
+								if (!rZero && !cMax && fields.get(field)[r-1][c+1])
+									count++;
+								if (!rMax && fields.get(field)[r+1][c])
+									count++;
+								if (!rMax && !cZero && fields.get(field)[r+1][c-1])
+									count++;
+								if (!rMax && !cMax && fields.get(field)[r+1][c+1])
+									count++;
+								if (!cZero && fields.get(field)[r][c-1])
+									count++;
+								if (!cMax && fields.get(field)[r][c+1])
+									count++;
+								System.out.print(count);
 							}
 						}
-						String[][] magic = new String[fields.get(field).length][fields.get(field)[0].length];
-						for (int r = 0; r < fields.get(field).length; r++){
-							for(int c = 0; c < fields.get(field)[0].length; c++){
-								magic[r][c] = (fields.get(field)[r][c] ? "T" : "F");
-								//System.out.print((fields.get(field)[r][c] ? "T" : "F"));
-							}
-						}
-						
-						for (int r = 0; r < magic.length; r++){
-							for(int c = 0; c < magic[0].length; c++){
-								if (magic[r][c].equals("T")){
-									System.out.print("*");
-								}else{
-									int count = 0;
-									boolean cZero = (c == 0);
-									boolean cMax = ((c + 1) == fields.get(field)[r].length);
-									boolean rZero = (r < 1);
-									boolean rMax = ((r + 1) == fields.get(field).length);
-									if (!rZero && magic[r-1][c].equals("T"))
-										count++;
-									if (!rZero && !cZero && magic[r-1][c-1].equals("T"))
-										count++;
-									if (!rZero && !cMax && magic[r-1][c+1].equals("T"))
-										count++;
-									if (!rMax && magic[r+1][c].equals("T"))
-										count++;
-									if (!rMax && !cZero && magic[r+1][c-1].equals("T"))
-										count++;
-									if (!rMax && !cMax && magic[r+1][c+1].equals("T"))
-										count++;
-									if (!cZero && magic[r][c-1].equals("T"))
-										count++;
-									if (!cMax && magic[r][c+1].equals("T"))
-										count++;
-									System.out.print(count);
-								}
-							}
-							System.out.println();
-						}
-						
-						field++;
+						System.out.println();
 					}
-				} catch (Exception e){}
-			
-		};
-	}
-	
-	private static Object p(Object p){
-		System.out.println(p); return p;
+					
+					field++;
+				}
+			} catch (Exception e){}			
+		}
 	}
 }
