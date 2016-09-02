caab5b5974ac5161bf4c1ee02d8b806054455d69
Kurtis Bowen <1750019@y115.org>
2016-09-02 14:25:54 -0500

    Basic Input/Structure

--- 1.6.7-Check The Check/src/Main.java
+++ 1.6.7-Check The Check/src/Main.java
@@ -1,11 +1,45 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		//TODO - Code
-
+		Scanner sc = new Scanner(System.in);
+		int gameNum = 0;
+		while (sc.hasNext()){
+			gameNum++;
+			char[][] board = new char[8][8];
+			for (int r = 0; r < 8; r++){
+				String input = sc.nextLine();
+				for (int c = 0; c < 8; c++){
+					board[r][c] = (char)input.getBytes()[c];
+				}
+			}
+			System.out.print("Game #" + gameNum + ": ");
+		}
+		sc.close();
 	}
-
+	
+	public static boolean checkPawn(char[][] board, int kingsRow, int kingsCollumn){
+		return false;
+	}
+	
+	public static boolean checkRook(char[][] board, int kingsRow, int kingsCollumn){
+		return false;
+	}
+	
+	public static boolean checkBishop(char[][] board, int kingsRow, int kingsCollumn){
+		return false;
+	}
+	
+	public static boolean checkQueen(char[][] board, int kingsRow, int kingsCollumn){
+		return false;
+	}
+	
+	public static boolean checkKing(char[][] board, int kingsRow, int kingsCollumn){
+		return false;
+	}
+	
+	public static boolean checkKnight(char[][] board, int kingsRow, int kingsCollumn){
+		return false;
+	}
 }
