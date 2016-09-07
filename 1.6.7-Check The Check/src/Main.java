e6f64de6b113a221d84233354adaa41b03541ffa
Kurtis Bowen <1750019@y115.org>
2016-09-07 14:32:17 -0500

    Kings Vs Kings

--- 1.6.7-Check The Check/src/Main.java
+++ 1.6.7-Check The Check/src/Main.java
@@ -5,21 +5,27 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int gameNum = 0;
-		while (sc.hasNext()){
+		while (sc.hasNext()) {
 			gameNum++;
 			piece[][] board = new piece[8][8];
-			
-			for (int r = 0; r < board.length; r++){
+
+			for (int r = 0; r < board.length; r++) {
 				String input = sc.nextLine();
-				for (int c = 0; c < board[0].length; c++){
-					char id = (char)input.getBytes()[c];
-					board[r][c] = new piece(id);
+				for (int c = 0; c < board[0].length; c++) {
+					char id = (char) input.getBytes()[c];
+					board[r][c] = new piece(id, r, c);
 				}
 			}
+
+			boolean kingDown = false;
+			boolean sideDown = false;// white = true; black = false;
+			for (piece[] row : board)
+				for (piece p : row) {
+					kingDown = kingDown || p.canKillKing(board);
+				}
+			System.out.print("Game #" + gameNum + ": " + (kingDown ? (sideDown ? "white king is in check." : "black king is in check.") : "no king is in check."));
 			sc.nextLine();
-			System.out.println("Game #" + gameNum + ": ");
-			board[0][0].canKillKing(board);
-			
+
 		}
 		sc.close();
 	}
--- 1.6.7-Check The Check/src/piece.java
+++ 1.6.7-Check The Check/src/piece.java
@@ -1,13 +1,18 @@
 
 public class piece {
-	boolean side; //True = caps
+	boolean side; //True = caps = white
 	boolean king;
 	char id;
 	
+	int r;
+	int c;
+	
 	String otherID;
 	
-	public piece(char id){
+	public piece(char id, int r, int c){
 		this.id = id;
+		this.c = c;
+		this.r = r;
 		side = ((int)id < 97); //if caps
 		king = (id == 'k' || id == 'K');
 		otherID = (id == 'p' || id == 'P' ? "pawn" : "");
@@ -24,9 +29,44 @@ public class piece {
 		return this.otherID;
 	}
 	
+	public boolean getSide(){
+		return this.side;
+	}
+	
+	public int getC(){
+		return this.c;
+	}
+	
+	public int getR(){
+		return this.r;
+	}
+	
 	public boolean canKillKing(piece[][] board){
+		boolean cZero = !(c > 0);
+		boolean cMax = (c >= 7);
+		boolean rZero = !(r > 0);
+		boolean rMax = (r >= 7);
+		
+		boolean canKill = false;
 		if (this.isKing()){
-			System.out.print("'Tis a King");
+			if (!cZero && !rZero)
+				canKill = canKill || (board[c-1][r-1].isKing() && (board[c-1][r-1].getSide() != this.getSide()));
+			if (!cZero)
+				canKill = canKill || (board[c-1][r].isKing() && (board[c-1][r].getSide() != this.getSide()));
+			if (!rZero)
+				canKill = canKill || (board[c][r-1].isKing() && (board[c][r-1].getSide() != this.getSide()));
+			if (!rZero && !cMax)
+				canKill = canKill || (board[c+1][r-1].isKing() && (board[c+1][r-1].getSide() != this.getSide()));
+			if (!cMax)
+				canKill = canKill || (board[c+1][r].isKing() && (board[c+1][r].getSide() != this.getSide()));
+			if (!rMax)
+				canKill = canKill || (board[c][r+1].isKing() && (board[c][r+1].getSide() != this.getSide()));
+			if (!rMax && !cZero)
+				canKill = canKill || (board[c-1][r+1].isKing() && (board[c-1][r+1].getSide() != this.getSide()));
+			if (!rMax && !cMax)
+				canKill = canKill || (board[c+1][r+1].isKing() && (board[c+1][r+1].getSide() != this.getSide()));
+			return canKill;
+			
 			//Check King
 		} else if (this.getID().equals("pawn")){
 			//check pawn
@@ -38,6 +78,6 @@ public class piece {
 			//check queen
 		}
 		
-		return true;
+		return canKill;
 	}
 }
