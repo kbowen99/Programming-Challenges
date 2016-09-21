1fd11f80cb48a8c910f4a6e521f88983aa2e18be
Kurtis Bowen <1750019@y115.org>
2016-09-21 14:35:17 -0500

    A Start

---
+++ 2.8.3-Hartals/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	/**
+	 * First value From Scanner - number of test cases
+	 */
+	static int Tval;
+	
+	/**
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (true){
+			/**
+			 * First value From Scanner - number of test cases
+			 */
+			Tval = sc.nextInt();
+			int magic = Tval;
+		}
+		sc.close();
+	}
+
+}
