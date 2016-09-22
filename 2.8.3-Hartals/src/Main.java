cea51302da4ae21f1dedfef20858fb7c26b547d1
Kurtis Bowen <1750019@y115.org>
2016-09-22 14:24:28 -0500

    Basic Input i Actually understand this now

--- 2.8.3-Hartals/src/Main.java
+++ 2.8.3-Hartals/src/Main.java
@@ -1,24 +1,52 @@
 import java.util.Scanner;
 
 public class Main {
-
-	/**
-	 * First value From Scanner - number of test cases
-	 */
-	static int Tval;
-	
-	/**
-	 * 
-	 */
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		while (true){
-			/**
-			 * First value From Scanner - number of test cases
+		/*
+		 * Tval = Number of cases
+		 */
+		int Tval = sc.nextInt();
+		for (int useless = 0; useless < Tval; useless++){
+			/*
+			 * nVals = Days to run
 			 */
-			Tval = sc.nextInt();
-			int magic = Tval;
+			int nVal = sc.nextInt();
+			/*
+			 * pVal = Number of Parties
+			 */
+			int pVal = sc.nextInt();
+			/*
+			 * Hartal Parameter (per party)
+			 */
+			int[] hVals = new int[pVal];
+			
+			//input Hvals
+			for (int i = 0; i < pVal; i++)
+				hVals[i] = sc.nextInt();
+			
+			/*
+			 * Hartals/Party
+			 */
+			String[][] schedule = new String[pVal][nVal];
+			
+			//Plan Strikes
+			for (int party = 0; party < pVal; party++)
+				for (int day = 0; day < nVal; day++)
+					schedule[party][day] = (day % hVals[party] == 0 ? "X" : "-");
+			
+			
+			
+			//Print Strikes
+			System.out.println();
+			for (int party = 0; party < pVal; party++){
+				System.out.print("Party: " + party + "|");
+				for (int day = 0; day < nVal; day++)
+					System.out.print(schedule[party][day]);
+				System.out.println();
+			}
+			
 		}
 		sc.close();
 	}
