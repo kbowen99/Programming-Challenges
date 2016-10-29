344155629e6a412d4cb638b90d9e5b9fef1b7929
kbowen99 <kurtisbowen99@gmail.com>
2016-10-29 15:42:55 -0500

    Cleaner Don't use strings to do the job of a char[]...

--- 3.8.3-Common Permutation/src/Main.java
+++ 3.8.3-Common Permutation/src/Main.java
@@ -8,15 +8,15 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		while (sc.hasNextLine()){
-			String line1 = sc.nextLine();
-			String line2 = sc.nextLine();
+			char[] line1 = sc.nextLine().toCharArray();
+			char[] line2 = sc.nextLine().toCharArray();
 			String Out = "";
-			for (int c1 = 0; c1 < line1.length(); c1++)
-				for(int c2 = 0; c2 < line2.length(); c2++)
-					if ((line1.toCharArray()[c1] == line2.toCharArray()[c2]) && !(Out.contains(line1.toCharArray()[c1] + "")))
-						Out += line1.toCharArray()[c1];
+			for (int c1 = 0; c1 < line1.length; c1++)
+				for(int c2 = 0; c2 < line2.length; c2++)
+					if ((line1[c1] == line2[c2]) && !(Out.contains(line1[c1] + "")))
+						Out += line1[c1];
 			String[] OutA = Out.split("");
-			Arrays.sort(OutA, Collator.getInstance(new Locale("en", "EN")));
+			Arrays.sort(OutA);
 			for (String c : OutA)
 				System.out.print(c);
 			System.out.println();
