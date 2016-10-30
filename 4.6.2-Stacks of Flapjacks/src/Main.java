6d0d2710cd6825705b9c6fc2ec905e0099d3596b
kbowen99 <kurtisbowen99@gmail.com>
2016-10-30 14:43:10 -0500

    I Dont Even

--- 4.6.2-Stacks of Flapjacks/src/Main.java
+++ 4.6.2-Stacks of Flapjacks/src/Main.java
@@ -17,10 +17,10 @@ public class Main {
 			numscan.close();
 			
 			//Find correct order of flappjacks
-			ArrayList<Integer> sortedFlapjacks = new ArrayList<>();
-			for (Integer I : pancakes)
-				sortedFlapjacks.add(I);
-			Collections.sort(sortedFlapjacks);
+			////ArrayList<Integer> sortedFlapjacks = new ArrayList<>();
+			////for (Integer I : pancakes)
+				////sortedFlapjacks.add(I);
+			////Collections.sort(sortedFlapjacks);
 			
 			//___________________
 			if (DEBUG){
@@ -28,18 +28,46 @@ public class Main {
 				p("Input FlapJacks:");
 				pList(pancakes);
 				p("Goal FlapJacks:");
-				pList(sortedFlapjacks);
+				////pList(sortedFlapjacks);
 			}
 			//___________________
 			
-			if (!listIsEqual(pancakes, sortedFlapjacks)){
+			int position = 0;
+//			p("Input Pancakes");
+//			pList(pancakes);
+			//Collections.reverse(pancakes);
+//			p("Curr Pancakes --------------");
+//			pList(pancakes);
+			
+			p(input);
+			
+			boolean isOr = !isOrderedList(pancakes);
+			String order = "";
+			while(isOr){
+				int maxIndex = 0;
+				int largest = 0;
+				for(int i = position; i < pancakes.size(); i++) {
+					if(pancakes.get(i) > largest) {
+						maxIndex = i;
+						largest = pancakes.get(i);
+					}
+				}
+				isOr = !isOrderedList(pancakes);
+				if(maxIndex != pancakes.size() - 1) {
+					pancakes = flip(pancakes, maxIndex);
+					order += (maxIndex + 1) + " ";
+				}
+				isOr = !isOrderedList(pancakes);
+				pancakes = flip(pancakes, position);
+				order += (position + 1) + " ";
+				position++;
+				isOr = !isOrderedList(pancakes);
 				
+				p("Curr Pancakes --------------");
+				pList(pancakes);
 			}
-			p("Input FlapJacks:");
-			pList(pancakes);
-			pancakes = flip(pancakes, 1);
-			p("Flipped FlapJacks:");
-			pList(pancakes);
+			order = (order.length() > 1 ? order.substring(0, order.length() - 2) : "1 ");
+			p(order + "0");
 		}
 	}
 	
@@ -48,15 +76,23 @@ public class Main {
 	}
 	
 	private static boolean listIsEqual(ArrayList<Integer> arr1, ArrayList<Integer> arr2){
-		boolean isEqual = true;
-		if (arr1.size() == arr2.size())
-			for (int i = 0; i < arr1.size(); i++)
-				if (arr1.get(i).intValue() != arr2.get(i).intValue())
-					isEqual = false;
-		else
-			isEqual = false;
-		
-		return isEqual;
+		String s1 = "";
+		for (Integer I : arr1)
+			s1 += I.intValue() + "";
+		String s2 = "";
+		for (Integer I : arr2)
+			s2 += I.intValue() + "";
+		return s1.equals(s2);
+	}
+	
+	private static boolean isOrderedList(ArrayList<Integer> mSort){
+		int last = mSort.get(0);
+		for (Integer currint : mSort){
+			if (currint > last)
+				return false;
+			last = currint;
+		}
+		return true;
 	}
 	
 	private static ArrayList<Integer> flip(ArrayList<Integer> toFlip, int FlipIndex){
