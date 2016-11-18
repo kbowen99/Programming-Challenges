3e85b980ec35c6a95179a9cb6f3f0389dbc820a6
Kurt Bowen <kurtisbowen99@gmail.com>
2016-11-18 14:38:49 -0600

    almost

--- 7.6.3-Euclids Problem/src/Main.java
+++ 7.6.3-Euclids Problem/src/Main.java
@@ -13,51 +13,36 @@ public class Main {
 				int B = sc.nextInt();
 				int D = gcd(A, B);
 				System.out.println(A + "X + " + B + "Y = " + D);
-				System.out.println(undoEuclid(A,B));
-				System.out.println("(" + X + "," + Y + ")");
-				System.out.println(D + "");
+				tryEuclid(A, B);
+				//System.out.println("(" + X + "," + Y + ")");
+				//System.out.println(D + "");
 			}
-		} catch (Exception e) {}
+		} catch (Exception e) {
+		}
 		sc.close();
 	}
-
+	
 	public static int gcd(int A, int B) {
 		return ((new BigInteger(A + "")).gcd(new BigInteger(B + ""))).intValue();
 	}
 	
-	public static int gcd2(int p, int q, int x, int y){
-		int x1 = 0,y1 = 0,g = 0;
-		
-		if (q > p) return (gcd2(q,p,y,x));
-		
-		if (q == 0) {
-			x = 1;
-			y = 0;
-			return p;
-		}
-		
-		g = gcd2(q, p%q, x1, y1);
-		x = y1;
-		y = (int) (x1 - Math.floor(p/q) * y1);
-		
-		return g;
+	public static int eGCD(int A, int B){
+		if (B>A)
+			return eGCD(B,A);
+		if (A%B > 0){
+			//System.out.println("GCD-Ing: " + A + " = " + (A/B) + " * " + B + " + " + (A%B));
+			//System.out.println("Maybe: " + (A%B) + " = " + A + " * -" + (A/B) + " " + B);
+			return eGCD(B,(A%B));
+		} else
+			return A/B;
 	}
 	
-	public static int undoEuclid(int a,int b)
-	{
-	    int x1 = 0,y1 = 0,d = 0;
-
-	    if(a==0){
-	    X=0;
-	    Y=1;
-	    return b;
-	    }
-
-	        d=undoEuclid(b%a,a);
-	        X=y1-(b/a)*x1;
-	        Y=x1;
-	        return d;
-
-
+	public static void tryEuclid(int A, int B){
+		int t = 2;
+		int D = gcd(A,B);
+		int X = t*A/D;
+		int Y = -t*B/D;
+		
+		System.out.println(A + "(" + X + ")" + "+" + B + "(" + Y + ") = " + D);
 	}
 }
