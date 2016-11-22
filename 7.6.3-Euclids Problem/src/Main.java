6d2a555aa153edeb66bee21384de58ce9d41fe6b
Kurt Bowen <kurtisbowen99@gmail.com>
2016-11-22 13:32:31 -0600

    I Broke it

--- 7.6.3-Euclids Problem/src/Main.java
+++ 7.6.3-Euclids Problem/src/Main.java
@@ -8,20 +8,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		try {
+		//try {
 			while (sc.hasNextLine()) {
-				int A = sc.nextInt();
-				int B = sc.nextInt();
-				int X = 0;
-				int Y = 0;
-				//System.out.println(A + "X + " + B + "Y = " + gcd(A, B));
-				Point XP = untilEqXP(A,B,X,Y);
-				Point YP = untilEqYP(A,B,X,Y);
-				System.out.print(((Math.abs(XP.getX()) + Math.abs(XP.getY())) < (Math.abs(YP.getX()) + Math.abs(YP.getY()))) ? (int)XP.getX() + " " + (int)XP.getY() : (int)YP.getX() + " " + (int)YP.getY());
-				System.out.println(" " + gcd(A,B));
+				BigInteger A = new BigInteger(sc.nextInt() + "");
+				BigInteger B = new BigInteger(sc.nextInt() + "");
+				BigInteger X = new BigInteger("0");
+				BigInteger Y = new BigInteger("0");
+				if (!A.equals(BigInteger.ZERO) && !B.equals(BigInteger.ZERO)){
+					int zero = 0;
+					Point XP = untilEqXP(A,B,X,Y,zero); zero = 0;
+					Point YP = untilEqYP(A,B,X,Y, zero);
+					System.out.println(XP + " " + YP);
+					//System.out.print(((Math.abs(XP.getX()) + Math.abs(XP.getY())) < (Math.abs(YP.getX()) + Math.abs(YP.getY()))) ? (int)XP.getX() + " " + (int)XP.getY() : (int)YP.getX() + " " + (int)YP.getY());
+				} else 
+					if (A.equals(BigInteger.ZERO) && !B.equals(BigInteger.ZERO))
+						System.out.print("0 1");
+					else if (B.equals(BigInteger.ZERO) && !A.equals(BigInteger.ZERO))
+						System.out.print("1 0");
+					else
+						System.out.print("0 0");
+				System.out.println(" " + A.gcd(B));
 			}
-		} catch (Exception e) {
-		}
+		//} catch (Exception e) {
+		//}
 		sc.close();
 	}
 	
@@ -29,27 +38,32 @@ public class Main {
 		return ((new BigInteger(A + "")).gcd(new BigInteger(B + ""))).intValue();
 	}
 	
-	public static Point untilEqXP(int A,int B, int X, int Y){
-		int D = gcd(A,B);
-		int LS = (A*X + B*Y);
-		if (LS == D)
-			return new Point(X,Y);
+	
+	public static Point untilEqXP(BigInteger A,BigInteger B, BigInteger X, BigInteger Y, int iteration){
+		iteration++;
+		BigInteger LS = (A.multiply(X)).add(B.multiply(Y));//(A*X + B*Y);
+		if (iteration > 200)
+			return null;
+		else if (LS == A.gcd(B))
+			return new Point(X.intValue(),Y.intValue());
 		else 
-			if (LS < D)
-				return untilEqXP(A, B, X + 1,Y);
+			if (LS.compareTo(A.gcd(B)) < 0)
+				return untilEqXP(A, B, X.add(BigInteger.ONE),Y,iteration);
 			else
-				return untilEqXP(A,B,X,Y - 1);
+				return untilEqXP(A,B,X,Y.subtract(BigInteger.ONE),iteration);
 	}
 	
-	public static Point untilEqYP(int A,int B, int X, int Y){
-		int D = gcd(A,B);
-		int LS = (A*X + B*Y);
-		if (LS == D)
-			return new Point(X,Y);
+	public static Point untilEqYP(BigInteger A,BigInteger B, BigInteger X, BigInteger Y, int iteration){
+		iteration++;
+		BigInteger LS = (A.multiply(X)).add(B.multiply(Y));//(A*X + B*Y);
+		if (iteration > 200)
+			return null;
+		else if (LS == A.gcd(B))
+			return new Point(X.intValue(),Y.intValue());
 		else 
-			if (LS < D)
-				return untilEqYP(A, B, X, Y + 1);
+			if (LS.compareTo(A.gcd(B)) < 0)
+				return untilEqYP(A, B, X, Y.add(BigInteger.ONE),iteration);
 			else
-				return untilEqYP(A,B,X - 1,Y);
+				return untilEqYP(A,B,X.subtract(BigInteger.ONE),Y,iteration);
 	}
 }
