8f562985205704bb8b38bb062d3e4831a6a6841d
kbowen99 <kurtisbowen99@gmail.com>
2016-12-05 21:19:42 -0600

    It Works, But not fast enough

--- 7.6.3-Euclids Problem/src/Main.java
+++ 7.6.3-Euclids Problem/src/Main.java
@@ -8,29 +8,26 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		//try {
+		try {
 			while (sc.hasNextLine()) {
-				BigInteger A = new BigInteger(sc.nextInt() + "");
-				BigInteger B = new BigInteger(sc.nextInt() + "");
-				BigInteger X = new BigInteger("0");
-				BigInteger Y = new BigInteger("0");
-				if (!A.equals(BigInteger.ZERO) && !B.equals(BigInteger.ZERO)){
-					int zero = 0;
-					Point XP = untilEqXP(A,B,X,Y,zero); zero = 0;
-					Point YP = untilEqYP(A,B,X,Y, zero);
-					System.out.println(XP + " " + YP);
-					//System.out.print(((Math.abs(XP.getX()) + Math.abs(XP.getY())) < (Math.abs(YP.getX()) + Math.abs(YP.getY()))) ? (int)XP.getX() + " " + (int)XP.getY() : (int)YP.getX() + " " + (int)YP.getY());
+				int A = sc.nextInt();
+				int B = sc.nextInt();
+				int D = gcd(A,B);
+				int X = 0;
+				int Y = 0;
+				if (A != 0 && B != 0){
+					Point XP = untilEqDir(A,B,X,Y,D,true);
+					Point YP = untilEqDir(A,B,X,Y,D,false);
+					System.out.print(((Math.abs(XP.getX()) + Math.abs(XP.getY())) < (Math.abs(YP.getX()) + Math.abs(YP.getY()))) ? (int)XP.getX() + " " + (int)XP.getY() : (int)YP.getX() + " " + (int)YP.getY());
 				} else 
-					if (A.equals(BigInteger.ZERO) && !B.equals(BigInteger.ZERO))
-						System.out.print("0 1");
-					else if (B.equals(BigInteger.ZERO) && !A.equals(BigInteger.ZERO))
+					if (A > 0)
 						System.out.print("1 0");
 					else
-						System.out.print("0 0");
-				System.out.println(" " + A.gcd(B));
+						System.out.print("0 1");
+				System.out.println(" " + gcd(A,B));
 			}
-		//} catch (Exception e) {
-		//}
+		} catch (Exception e) {
+		}
 		sc.close();
 	}
 	
@@ -38,32 +35,60 @@ public class Main {
 		return ((new BigInteger(A + "")).gcd(new BigInteger(B + ""))).intValue();
 	}
 	
-	
-	public static Point untilEqXP(BigInteger A,BigInteger B, BigInteger X, BigInteger Y, int iteration){
-		iteration++;
-		BigInteger LS = (A.multiply(X)).add(B.multiply(Y));//(A*X + B*Y);
-		if (iteration > 200)
-			return null;
-		else if (LS == A.gcd(B))
-			return new Point(X.intValue(),Y.intValue());
+	/**
+	 * AX + BY = D
+	 * Recursive Method for finding logical points in either "Y-Positive" (True) or "X-Positive" (false) "searching"
+	 * Who Would've guessed Java only has a 512kb stack size? Who also would've guessed this implementation doesn't always work?
+	 * @param A - Provided 'A' Value
+	 * @param B - Provided 'B' Value
+	 * @param X - Calculating 'X' Value
+	 * @param Y - Calculating 'Y' Value
+	 * @param D - Greatest Common Divisor of A & B (Recalculating every time increases stack)
+	 * @param Dir true - XP; false - YP;
+	 * @return Calculated Point
+	 */
+	@Deprecated
+	public static Point untilEqDir(int A,int B, int X, int Y, int D, boolean Dir){
+		int LS = (A*X + B*Y);
+		if (LS == D)
+			return new Point(X,Y);
 		else 
-			if (LS.compareTo(A.gcd(B)) < 0)
-				return untilEqXP(A, B, X.add(BigInteger.ONE),Y,iteration);
+			if (LS < D)
+				return (Dir ? untilEqDir(A, B, X + 1,Y, D, Dir) : untilEqDir(A, B, X, Y + 1, D, Dir));
 			else
-				return untilEqXP(A,B,X,Y.subtract(BigInteger.ONE),iteration);
+				return (Dir ? untilEqDir(A,B,X,Y - 1, D, Dir) : untilEqDir(A,B,X - 1,Y, D, Dir));
 	}
 	
-	public static Point untilEqYP(BigInteger A,BigInteger B, BigInteger X, BigInteger Y, int iteration){
-		iteration++;
-		BigInteger LS = (A.multiply(X)).add(B.multiply(Y));//(A*X + B*Y);
-		if (iteration > 200)
-			return null;
-		else if (LS == A.gcd(B))
-			return new Point(X.intValue(),Y.intValue());
-		else 
-			if (LS.compareTo(A.gcd(B)) < 0)
-				return untilEqYP(A, B, X, Y.add(BigInteger.ONE),iteration);
-			else
-				return untilEqYP(A,B,X.subtract(BigInteger.ONE),Y,iteration);
+	/**
+	 * AX + BY = D
+	 * Non-Recursive Method for finding logical points in either "Y-Positive" (True) or "X-Positive" (false) "searching"
+	 * @param A - Provided 'A' Value
+	 * @param B - Provided 'B' Value
+	 * @param X - Calculating 'X' Value
+	 * @param Y - Calculating 'Y' Value
+	 * @param D - Greatest Common Divisor of A & B (Recalculating every time increases stack)
+	 * @param Dir true - XP; false - YP;
+	 * @return Calculated Point
+	 */
+	public static Point tilEqDirLoop(int A,int B, int X, int Y, int D, boolean Dir){
+		int mS = 1000000001;
+		while (true){
+			if (Math.abs(X) > mS || Math.abs(Y) > mS)
+				return null;
+			int LS = (A*X + B*Y);
+			if (LS == D)
+				return new Point(X,Y);
+			else 
+				if (LS < D)
+					if (Dir)
+						X++;
+					else
+						Y++;
+				else
+					if (Dir)
+						Y--;
+					else
+						X--;
+		}
 	}
 }
