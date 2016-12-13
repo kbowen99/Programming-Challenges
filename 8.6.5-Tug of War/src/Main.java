aaca65604846f14673a4d5c1a30a90cdc3b76c3a
Kurt Bowen <kurtisbowen99@gmail.com>
2016-12-13 14:37:00 -0600

    All the Sorts

--- 8.6.5-Tug of War/src/Main.java
+++ 8.6.5-Tug of War/src/Main.java
@@ -14,17 +14,28 @@ public class Main {
 			int numPeople = sc.nextInt();
 			for (int p = 0; p < numPeople; p++)
 				peoples.add(sc.nextInt());
+			ArrayList<Integer> peoples2 = new ArrayList<>(peoples);
 			Collections.sort(peoples);
-			for (int i : peoples)
-				System.out.println(i);
-			System.out.println(newSort(t1,t2,peoples,true));
+			String nS = newSort(t1,t2,peoples,true);
+			String lS = lazySort(peoples2);
+			
+			System.out.println(pSort(nS,lS) + "\n");
 		}
 		sc.close();
 	}
 	
+	public static String pSort(String o1, String o2){
+		int o1d = Math.abs(Integer.parseInt(o1.substring(0, o1.indexOf(" "))) - Integer.parseInt(o1.substring(o1.indexOf(" ") + 1)));
+		int o2d = Math.abs(Integer.parseInt(o2.substring(0, o2.indexOf(" "))) - Integer.parseInt(o2.substring(o2.indexOf(" ") + 1)));
+		return (o1d < o2d ? o1 : o2);
+	}
+	
 	public static String newSort(ArrayList<Integer> Team1, ArrayList<Integer> Team2, ArrayList<Integer> people, boolean dir){
-		if (people.size() == 0) //No More Weight
-			return getTeamWeight(Team1) + " " + getTeamWeight(Team2) + "\n " + Team1.size() + " | " + Team2.size();
+		if (people.size() == 0){ //No More Weight
+			reSort(Team1, Team2, false);
+			reSort(Team1, Team2, false);
+			return (getTeamWeight(Team1) < getTeamWeight(Team2) ? getTeamWeight(Team1) + " " + getTeamWeight(Team2) : getTeamWeight(Team2) + " " + getTeamWeight(Team1));
+		}
 		if (dir){ //T1 gains Weight
 			if (getTeamWeight(Team1) > getTeamWeight(Team2))//T1 Gains Little Weight
 				Team1.add(people.remove(0));
@@ -39,7 +50,23 @@ public class Main {
 			return newSort(Team1,Team2,people,!dir);
 		}
 	}
-
+	
+	private static boolean reSort(ArrayList<Integer> Team1, ArrayList<Integer> Team2, boolean f){
+		int d = Math.abs(getTeamWeight(Team1) - getTeamWeight(Team2));
+		for (int T1 = 0; T1 < Team1.size(); T1++)
+			for (int T2 = 0; T2 < Team2.size(); T2++){
+				int nT1w = (getTeamWeight(Team1) - Team1.get(T1) + Team2.get(T2));
+				int nT2w = (getTeamWeight(Team2) - Team2.get(T2) + Team1.get(T1));
+				if (Math.abs(nT1w - nT2w) < d){
+					Team2.add(Team1.remove(T1));
+					Team1.add(Team2.remove(T2));
+					return reSort(Team1, Team2, false);
+				}
+			}
+		if (!f)
+			return reSort(Team2, Team1, true);
+		return false;
+	}
 	
 	private static int getTeamWeight(ArrayList<Integer> team){
 		int total = 0;
@@ -48,39 +75,11 @@ public class Main {
 		return total;
 	}
 	
-	//----------------------------------Bad Code Abyss
 	@Deprecated
-	private static String sorter(ArrayList<Integer> Team1, ArrayList<Integer> Team2, ArrayList<Integer> people, boolean dir){
-		if (people.size() == 0){
-			//makeAmericaGreatAgain(Team1,Team2);
-			return getTeamWeight(Team1) + " " + getTeamWeight(Team2);
-		} else
-			if (T2T(Team1,Team2,people))
-				return sorter(Team1,I2A(people.remove(people.size() - 1), Team2), people, dir);
-			else
-				return sorter(I2A(people.remove(people.size() - 1), Team1),Team2, people, dir);
-	}
-	@Deprecated
-	private static ArrayList<Integer> I2A(int toAdd, ArrayList<Integer> arr){
-		arr.add(toAdd);
-		return arr;
-	}
-	@Deprecated
-	private static boolean T2T(ArrayList<Integer> T1, ArrayList<Integer> T2, ArrayList<Integer> p){
-		int DeltaT1Gains = Math.abs((getTeamWeight(T1) + p.get(p.size() - 1)) - getTeamWeight(T2));
-		int DeltaT2Gains = Math.abs(getTeamWeight(T1) - (getTeamWeight(T2) + p.get(p.size() - 1)));
-		return DeltaT1Gains >= DeltaT2Gains;
-	}
-	@Deprecated
-	private static boolean makeAmericaGreatAgain(ArrayList<Integer> t1, ArrayList<Integer> t2){
-		Collections.sort(t1);
-		Collections.sort(t2);
-		if (Math.abs(t1.size() - t2.size()) < 2){return true;} else {
-			if (t1.size() > t2.size()){
-				t2.add(t1.remove(0));
-				return makeAmericaGreatAgain(t1, t2);
-			} else 
-				return makeAmericaGreatAgain(I2A(t2.remove(0), t1), t2);
-		}
+	public static String lazySort(ArrayList<Integer> people){
+		ArrayList<Integer> Team1 = new ArrayList<Integer>(people.subList(0, people.size() / 2));
+		ArrayList<Integer> Team2 = new ArrayList<Integer>(people.subList(people.size() / 2, people.size()));
+		reSort(Team1, Team2, false);
+		return (getTeamWeight(Team1) < getTeamWeight(Team2) ? getTeamWeight(Team1) + " " + getTeamWeight(Team2) : getTeamWeight(Team2) + " " + getTeamWeight(Team1));
 	}
 }
