84559bf6aa9e84a72acf1fc94554af6c06a7a263
kbowen99 <kurtisbowen99@gmail.com>
2016-12-18 23:53:40 -0600

    commit

--- 2.8.1-Jolly Jumpers/src/Main.java
+++ 2.8.1-Jolly Jumpers/src/Main.java
@@ -1,106 +1,24 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-	final static boolean DEBUG = false;
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		while (sc.hasNext()) {
-			String input = sc.nextLine();
-			Scanner numScan = new Scanner(input);
-			// numScan.useDelimiter("[^0-9]+");
-			numScan.nextInt();
-			ArrayList<Integer> nums = new ArrayList<>();
-			ArrayList<Integer> diffs = new ArrayList<>();
-
-			// input numbers
-			while (numScan.hasNextInt())
-				nums.add(numScan.nextInt());
-
-			// calculate diffs
-			int lastDiff = Integer.MAX_VALUE;
-			for (int i = 1; i < nums.size(); i++) {
-				int currScan = nums.get(i);
-				int lastScan = nums.get(i - 1);
-				int diff = Math.abs(lastScan - currScan);
-				lastDiff = diff;
-				addLogically(diffs, lastDiff);
+			while (sc.hasNext()) {
+				int[] jumpers = new int[sc.nextInt()];
+				boolean[] taken = new boolean[jumpers.length - 1];
+				// fill the array
+				for (int i = 0; i < jumpers.length; i++)
+					jumpers[i] = sc.nextInt();
+				for (int i = 1; i < jumpers.length; i++)
+					if ((Math.abs(jumpers[i - 1] - jumpers[i]) - 1) >= 0 && (Math.abs(jumpers[i - 1] - jumpers[i]) - 1) < taken.length)
+						taken[(Math.abs(jumpers[i - 1] - jumpers[i]) - 1)] = true;
+				boolean o = false;
+				for (boolean b : taken)
+					o = o || !b;
+				System.out.println((o ? "Not jolly" : "Jolly"));
 			}
-
-			// check diffs
-			if (nums.size() > 1)
-				System.out.println((isJolly(diffs) ? "Jolly" : "Not jolly"));
-			else
-				System.out.println("Jolly");
-
-			if (DEBUG) {
-				System.out.println("INPUT:");
-				for (int i : nums)
-					System.out.print(i + " ");
-				System.out.println();
-				System.out.println("OUTPUT:");
-				for (int i : diffs)
-					System.out.print(i + " ");
-				System.out.println();
-				System.out.println();
-			}
-			// if (nums.size() == 1)
-			// System.out.println("Jolly");
-			// else {
-			// int lastDiff = Integer.MAX_VALUE;
-			// for (int i = 1; i < nums.size(); i++) {
-			// int currScan = nums.get(i);
-			// int lastScan = nums.get(i - 1);
-			// int diff = Math.abs(lastScan - currScan);
-			// if (diff > lastDiff) {
-			// System.out.println("Not jolly");
-			// break;
-			// }
-			// if (i == nums.size() - 1)
-			// System.out.println("Jolly");
-			// lastDiff = diff;
-			// }
-			// }
-
-			numScan.close();
-		}
 		sc.close();
-	}
-
-	private static void addLogically(ArrayList<Integer> addTo, int addee) {
-		if (addTo.size() == 0) {
-			addTo.add(addee);
-		} else {
-			for (int i = 0; i < addTo.size(); i++) {
-				if (addee > addTo.get(i)) {
-					addTo.add(i, addee);
-					break;
-				}
-			}
-			if (!addTo.contains(addee)) {
-				addTo.add(addee);
-			}
-		}
-
-	}
-
-	private static boolean isJolly(ArrayList<Integer> diffs) {
-		if (diffs.size() == 0)
-			return true;		
-		if (!diffs.contains(1))
-			return false;
-		if (diffs.size() > 0) {
-			int lastDiff1 = diffs.get(0);
-			for (int check : diffs) {
-				if (!(check == lastDiff1 || check == (lastDiff1 - 1))) {
-					return false;
-				}
-				lastDiff1 = check;
-			}
-		}
-		return true;
 	}
 
 }
